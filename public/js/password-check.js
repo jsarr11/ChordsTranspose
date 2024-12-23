d66f02c9c2baf22db7906242bf6a667eaefeaa0c
johns <jsarris11@gmail.com>
2024-12-23 19:27:18 +0200

    remember session

--- public/js/password-check.js
+++ public/js/password-check.js
@@ -8,10 +8,20 @@ const errorMessage = document.getElementById("error-message");
 const passwordContainer = document.getElementById("password-container");
 const content = document.getElementById("content");
 
+// Check if the user is already authenticated in this session
+if (sessionStorage.getItem("authenticated") === "true") {
+    // If authenticated, show the content directly
+    passwordContainer.style.display = "none";
+    content.style.display = "block";
+}
+
 // Add event listener for password submission
 submitButton.addEventListener("click", () => {
     const enteredPassword = passwordInput.value;
     if (enteredPassword === PASSWORD) {
+        // Save authentication status in Session Storage
+        sessionStorage.setItem("authenticated", "true");
+
         // Hide password container and show the content
         passwordContainer.style.display = "none";
         content.style.display = "block";
