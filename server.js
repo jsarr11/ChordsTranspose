28556f88c7885c1c862cce04fc864db834ed1eeb
johns <jsarris11@gmail.com>
2024-12-23 15:45:24 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Import routes
+const indexRouter = require('./routes/index');
+app.use('/api', indexRouter);
+
+// Default route to serve the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
